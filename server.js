ad4880e412fbf5f618cd64b43a66fc765c3c0f03
KacperRybak <224421@edu.p.lodz.pl>
2021-04-03 16:39:59 +0200

    test

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const postRouter = require('./routes/posts')
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use('/posts', postRouter)
+
+app.get('/', (req, res) =>{
+    const posts = [{
+        title: 'Test post',
+        createdAt: Date.now(),
+        description: 'Test description'
+    }]
+    res.render('index', {posts: posts})
+})
+
+app.listen(5000)
