bc91466199c19a4a72c11913405b232d829c555c
KacperRybak <224421@edu.p.lodz.pl>
2021-04-09 16:54:37 +0200

    Koniec poradnika

--- routes/posts.js
+++ routes/posts.js
@@ -6,27 +6,43 @@ router.get('/new', (req, res)=>{
     res.render('posts/new', {post: new Post() })
 })
 
+router.get('/edit/:id', async (req, res)=>{
+    const post = await Post.findById(req.params.id)
+    res.render('posts/edit', {post: post })
+})
+
 router.get('/:slug', async (req, res)=>{
     const post = await Post.findOne({ slug: req.params.slug })
     if(post == null) res.redirect('/')
     res.render('posts/show', {post: post})
 })
 
-router.post('/', async (req, res)=>{
-    let post = new Post({
-        title: req.body.title,
-        description: req.body.description,
-        markdown: req.body.markdown
-    })
-    try{
-        post = await post.save()
-        let str = '/posts/'
-        str += post.slug
-        res.redirect(str)
-    }catch(e){
-        res.render('posts/new', {post: post})
+router.post('/', async (req, res, next)=>{
+    req.post = new Post()
+    next()
+}, savePostAndRedirect('new'))
+
+router.put('/:id', async (req, res, next)=>{
+    req.post = await Post.findById(req.params.id)
+    next()
+}, savePostAndRedirect('edit'))
+
+function savePostAndRedirect(path){
+    return async (req, res) =>{
+        let post =req.post
+        post.title = req.body.title
+        post.description = req.body.description
+         post.markdown = req.body.markdown
+        try{
+            post = await post.save()
+            let str = '/posts/'
+            str += post.slug
+            res.redirect(str)
+        }catch(e){
+            res.render('posts/${path}', {post: post})
+        }
     }
-})
+}
 
 method="DELETE"
 router.delete('/:id', async (req,res) => {
