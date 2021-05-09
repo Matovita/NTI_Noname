196019d654124fec4de2fc1e2c6059eb4cdde7d9
KamilBory <73657592+KamilBory@users.noreply.github.com>
2021-05-10 00:23:49 +0200

    Authentication mock implementation

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express')
 const mongoose = require('mongoose')
 const Post = require('./models/post')
 const postRouter = require('./routes/posts')
+// const User = require('./models/user')
+// const bcrypt = require('bcryptjs')
+// const jwt = require('jsonwebtoken')
 const methodOverride = require('method-override')
+//const bodyParser = require('body-parser')
 const app = express()
+
+//const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
 
 mongoose.connect('mongodb://localhost/db', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
 
@@ -15,6 +21,69 @@ app.get('/', async (req, res) =>{
     const posts = await Post.find().sort({createdAt: 'desc'})
     res.render('posts/index', {posts: posts})
 })
+
+// app.post('/', async (req, res) => {
+// 	const { username, password } = req.body
+// 	const user = await User.findOne({ username }).lean()
+
+// 	if (!user) {
+// 		return res.json({ status: 'error', error: 'Invalid username/password' })
+// 	}
+
+// 	if (await bcrypt.compare(password, user.password)) {
+// 		// the username, password combination is successful
+
+// 		const token = jwt.sign(
+// 			{
+// 				id: user._id,
+// 				username: user.username
+// 			},
+// 			JWT_SECRET
+// 		)
+
+// 		return res.json({ status: 'ok', data: token })
+// 	}
+
+// 	res.json({ status: 'error', error: 'Invalid username/password' })
+// })
+
+// app.post('/', async (req, res) => {
+// 	const { username, password: plainTextPassword } = req.body
+
+// 	if (!username || typeof username !== 'string') {
+// 		return res.json({ status: 'error', error: 'Invalid username' })
+// 	}
+
+// 	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+// 		return res.json({ status: 'error', error: 'Invalid password' })
+// 	}
+
+// 	if (plainTextPassword.length < 5) {
+// 		return res.json({
+// 			status: 'error',
+// 			error: 'Password too small. Should be atleast 6 characters'
+// 		})
+// 	}
+
+// 	const password = await bcrypt.hash(plainTextPassword, 10)
+
+// 	try {
+// 		const response = await User.create({
+// 			username,
+// 			password
+// 		})
+// 		console.log('User created successfully: ', response)
+// 	} catch (error) {
+// 		if (error.code === 11000) {
+// 			// duplicate key
+// 			return res.json({ status: 'error', error: 'Username already in use' })
+// 		}
+// 		throw error
+// 	}
+
+// 	res.json({ status: 'ok' })
+// })
+
 
 app.use('/posts', postRouter)
 
