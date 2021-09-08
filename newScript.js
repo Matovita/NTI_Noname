e78d9f0c31b6dc5abe1a947ee664e24cdd9c4c10
Matovita <mateusz.dela.md@gmail.com>
2021-09-08 06:55:36 +0200

    much closer

--- newScript.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function changemysize(value) {
-    const texts = document.getElementsByClassName(`text-change`);
-    
-    for (let i = 0; i < texts.length; i++) {
-        texts[i].style.fontSize = value + `px`; 
-    }
-}
